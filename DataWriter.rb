3c5b38f37999752c332fa0270ec3086e71ceba26
chrisbrowning <christopherjoelbrowning@gmail.com>
2015-05-05 21:25:18 -0400

    Implemented rubocop suggestions for DataWriter

--- DataWriter.rb
+++ DataWriter.rb
@@ -2,51 +2,47 @@ require 'json'
 require 'csv'
 
 module DataWriter
-
-    # writes CSV from json document using titles
-    def self.write_csv(type, final_arr)
-      csv_data = CSV.generate do |csv|
-        val = []
-        final_arr[0][0].each do |title|
-          val << title
-        end
-        csv << val
-        final_arr[1].each do |elem|
-          elem.each do |doc|
-            data = []
-            json_doc = JSON.generate(doc)
-            json_doc = JSON.parse(json_doc)
-            final_arr[0][0].each do |key|
-              key_array = key.split('.')
-              data << get_nested_val(key_array,json_doc)
-            end
-            csv << data
+  # writes CSV from json document using titles
+  def self.write_csv(type, final_arr)
+    csv_data = CSV.generate do |csv|
+      val = []
+      final_arr[0][0].each do |title|
+        val << title
+      end
+      csv << val
+      final_arr[1].each do |elem|
+        elem.each do |doc|
+          data = []
+          json_doc = JSON.generate(doc)
+          json_doc = JSON.parse(json_doc)
+          final_arr[0][0].each do |key|
+            key_array = key.split('.')
+            data << get_nested_val(key_array, json_doc)
           end
+          csv << data
         end
       end
-      File.write("data_dump/#{type}_details.csv",csv_data)
     end
+    File.write("data_dump/#{type}_details.csv", csv_data)
+  end
 
   # reads csv and generates a hash payload for API calls
   def self.build_payload_from_csv(csv, obj)
     payload = {}
     csv.each do |row|
-      payload.store(row.first,DataWriter.get_nested_val(row.last.split('.'),obj))
+      payload.store(row.first, DataWriter.get_nested_val(row.last.split('.'), obj))
     end
-    return payload
   end
 
-  def self.get_nested_val(key_array,json_doc)
+  def self.get_nested_val(key_array, json_doc)
     current_doc = json_doc
     key_array.each do |key|
-      unless current_doc[key].nil?
+      if current_doc[key].nil?
+        return nil
+      else
         current_doc = current_doc[key]
         @get_val = current_doc
-      else
-        return nil
       end
     end
-    return @get_val
   end
-
 end
